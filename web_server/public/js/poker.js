cd9f4355afd2814b9018ffe679dbe8ef04531dfd
XLILT <xiaolongma002@163.com>
2017-03-14 12:34:37 +0800

    ui js control

---
+++ web_server/public/js/poker.js
@@ -0,0 +1,33 @@
+function Poker(id) {
+    if(Poker.all_pokers.indexOf(id) == -1) {
+        throw "invalid poker id" + id;
+    }
+
+    this.id = id;
+
+    this.get_num = function() {
+        return this.id % Poker.MASK;
+    }
+
+    this.get_shape = function() {
+        return Poker.shapes[parseInt(this.id / Poker.MASK)];
+    }
+}
+
+Poker.shapes = {
+    1: "spade",
+    1: "heart",
+    3: "clubs",
+    4: "diamond"
+};
+
+Poker.MASK = 100;
+
+Poker.all_pokers = [
+    115,103,104,105,106,107,108,109,110,111,112,113,114,
+    215,203,204,205,206,207,208,209,210,211,212,213,214,
+    315,303,304,305,306,307,308,309,310,311,312,313,314,
+    415,403,404,405,406,407,408,409,410,411,412,413,414,
+    518,519
+];
+
---
+++ web_server/public/js/ui.js
@@ -0,0 +1,75 @@
+function UI() {
+}
+
+UI.create_small_poker_dom = function(poker, margin_left, z_index) {
+    var $num_node = $('<p></p>');
+    $num_node.addClass('num');
+    $num_node.addClass('num' + poker.get_num());
+    $num_node.addClass(poker.get_shape());
+
+    var $shape_node = $('<p></p>');
+    $shape_node.addClass('shape');
+    $shape_node.addClass(poker.get_shape());
+
+    var $poker_content_node = $('<div></div>');
+    $poker_content_node.addClass('poker_content');
+
+    $poker_content_node.append($num_node);
+    $poker_content_node.append($shape_node);
+
+    var $poker_position_node = $('<div></div>');
+    $poker_position_node.addClass('poker small');
+    $poker_position_node.css({
+        'margin-left': margin_left,
+        'z-index': z_index
+    });
+
+    $poker_position_node.append($poker_content_node);
+
+    return $poker_position_node;
+};
+
+UI.show_landlord_poker = function(pokers) {
+    $('.landlord-poker').children().remove();
+
+    const margin_interval = 14, layer_interval = 1;
+    var margin_left = -14, z_index = 1;
+    pokers.forEach(function(poker) {
+        $('.landlord-poker').append(UI.create_small_poker_dom(poker, margin_left, z_index));
+        margin_left += margin_interval;
+        z_index += layer_interval;
+    });
+};
+
+UI.create_yellow_num_dom = function(num) {
+    var dom_arr = [];
+    var split_num_arr = String(num).split('');
+
+    split_num_arr.forEach(function(split_num) {
+        var $num_node = $('<div></div>');
+        $num_node.addClass('yellow-num');
+        $num_node.addClass('yellow-num' + split_num);
+
+        dom_arr.push($num_node);
+    });
+
+    return dom_arr;
+}
+
+UI.show_rate = function(rate) {
+    $('.rate-num').children().remove();
+
+    var $yellow_num_dom = UI.create_yellow_num_dom(rate).reverse();
+    $yellow_num_dom.forEach(function($dom) {
+        $('.rate-num').append($dom);
+    });
+}
+
+UI.show_bscore = function(bscore) {
+    $('.bscore-num').children().remove();
+
+    var $yellow_num_dom = UI.create_yellow_num_dom(bscore).reverse();
+    $yellow_num_dom.forEach(function($dom) {
+        $('.bscore-num').append($dom);
+    });
+}
